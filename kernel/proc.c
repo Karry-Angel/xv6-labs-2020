2aca7ad59278177139eb87658b14df069499a552
Karry-Angel <2271576069@qq.com>
2023-07-26 13:47:49 +0800

    commit the second lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -294,6 +294,9 @@ fork(void)
   pid = np->pid;
 
   np->state = RUNNABLE;
+  
+   // 子进程复制父进程的 mask 
+  np->mask = p->mask;
 
   release(&np->lock);
 
@@ -693,3 +696,27 @@ procdump(void)
     printf("\n");
   }
 }
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
+}
+
