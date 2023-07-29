afb6d2761b7a94e11c3b2c4c172f0a722ffb6139
Karry-Angel <2271576069@qq.com>
2023-07-29 22:01:53 +0800

    finish lab2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -7,6 +7,7 @@
 
 #define NBUCKET 5
 #define NKEYS 100000
+pthread_mutex_t locks[NBUCKET];   //add
 
 struct entry {
   int key;
@@ -46,6 +47,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(locks + i);  // (+)add
   if(e){
     // update the existing key.
     e->value = value;
@@ -53,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks+i);//add
 }
 
 static struct entry*
@@ -99,6 +102,9 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  for (int i = 0; i < NBUCKET; i++) {  //add
+        pthread_mutex_init(locks + i, NULL);      //add
+    }//给每个散列加一把锁      //add
   pthread_t *tha;
   void *value;
   double t1, t0;
